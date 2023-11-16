d09bd9dd0277fb5c69a9ec991733abe3d7b8d339
Geovani França <geovanifranca76@gmail.com>
2023-11-16 16:14:35 -0300

    Te amo sarah

---
+++ teste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste
+    </title>
+</head>
+<body>
+    <h1>exemplo 2</h1>
+
+    <?php
+    $x=5;
+    $y=4;
+
+    echo"X=$x";
+    echo" <br> Y=$y";
+    echo"<br> x+y=".$x + $y;
+    ?>
+
+</body>
+</html>
